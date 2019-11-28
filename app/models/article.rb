878f7eec9e10cf8a4af3fdc2eaa71341df8e0c4a
odennkunn <y.taniguchi920@gmail.com>
2019-11-28 16:52:10 +0900

    carrierwaveの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,8 @@ class Article < ApplicationRecord
 
   belongs_to :user
 
+  mount_uploader :image, ImageUploader
+
   enum category: { Cook: 1, Beauty: 2, Clean: 3, Other: 4 }, _prefix: true
+  
 end
