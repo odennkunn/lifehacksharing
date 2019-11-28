ae6ef195071fb65ef86c365d1faf1ad5652de031
odennkunn <y.taniguchi920@gmail.com>
2019-11-28 10:23:39 +0900

    post function create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,21 @@ class ArticlesController < ApplicationController
   def index
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :image, :text, :category).merge(user_id: current_user.id)
+  end
 end
