f560650ff1a1e3c2d34ed5ccb0a0c5a79e347fdb
odennkunn <y.taniguchi920@gmail.com>
2019-11-29 21:20:31 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  constraints ->  request { request.session[:user_id].present? } do
-    root to: "articles#index"
-  end
-
   root to: 'top#index'
 
   resources :articles do
