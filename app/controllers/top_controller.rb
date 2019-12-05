947623abd81ce6155ebf1734fbfd63f2233d8b19
odennkunn <y.taniguchi920@gmail.com>
2019-12-05 11:09:18 +0900

    Revert "ルーティング修正"
    This reverts commit ea4bd3a5ebe689765b4f4650193de9f9c1dcb754.

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,17 +1,6 @@
 class TopController < ApplicationController
 
-  before_action :user_decision, only: [:index]
-
   def index 
   end
   
-
-  private
-
-  def user_decision
-    if user_signed_in?
-      redirect_to articles_path
-    end
-  end
-
 end
