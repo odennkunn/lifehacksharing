af5b9bc466f0f3e6e7e13abc7fcd3a67c7451bcc
odennkunn <y.taniguchi920@gmail.com>
2019-12-05 11:13:24 +0900

    ログイン済みがどうかで判定

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,17 @@
 class TopController < ApplicationController
 
+  before_action :user_decision, only: [:index]
+
   def index 
+  end
+  
+
+  private
+
+  def user_decision
+    if user_signed_in?
+      redirect_to articles_path
+    end
   end
   
 end
