e316f0132fef5d86839ed357514476a6a87f27f1
odennkunn <y.taniguchi920@gmail.com>
2019-12-05 16:42:36 +0900

    add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
 
   has_many :articles
   has_many :comments
+
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :nickname, presence: true, uniqueness: true, length: { maximum: 10 }
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: "フォーマットが不適切です"}
+  
 end
