bbca84441212c34e3b97f1a23d17e5d815fe0ea0
odennkunn <y.taniguchi920@gmail.com>
2019-12-06 10:25:15 +0900

    format振り分け

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,11 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
   def create
-    comment = Comment.create(text: comment_params[:text], article_id: comment_params[:article_id], user_id: current_user.id)
-    redirect_to "/articles/#{comment.article.id}"
+    @comment = Comment.create(text: comment_params[:text], article_id: comment_params[:article_id], user_id: current_user.id)
+    respond_to do |format|
+      format.html { redirect_to article_path(params[:article_id]) }
+      format.json
+    end
   end
 
   private
