c3422d10997c01d87bd73bf21bcccbafd631372f
odennkunn <y.taniguchi920@gmail.com>
2019-12-07 22:07:24 +0900

    my-like-index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
-  
+
   def show
     user = User.find(params[:id])
     @articles = user.articles.page(params[:page]).per(8).order("created_at DESC")
   end
-
 
 end
